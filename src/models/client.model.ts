06d8d9cb7bc4f3cca590f374c836bf3a35bc5b12
Maximenya <maximenya.deo@gmail.com>
2018-07-23 19:09:03 +0300

    update client mongo scheme

--- src/models/client.model.ts
+++ src/models/client.model.ts
@@ -1,20 +1,50 @@
 import { Schema, model } from "mongoose";
 
-const clientSchema = new Schema({
+const SubscriptionSchema = new Schema({
+    type: {type: Number},
+    saleTime: {type: Date},
+    firstDate: {type: Date},
+    lastDate: {type: Date},
+    counter: {type: Number},
+    current: {type: Number},
+    fancyName: {type: String}
+});
+
+const RentSchema = new Schema({
+    climbingShoes: {type: Number},
+    harness: {type: Number},
+    magnesia: {type: Number},
+    carabine: {type: Number},
+    griGri: {type: Number}
+});
+
+const ServiceHistorySchema = new Schema({
+    type: {type: Number},
+    checkIn: {type: Number},
+    checkOut: {type: Number},
+    keyNumber: {type: Number},
+    saleTime: {type: Number},
+    fancyName: {type: Number},
+    rent: {type: RentSchema}
+});
+
+const ClientSchema = new Schema({
     phone: {type: String},
     firstName: {type: String},
     middleName: {type: String},
     lastName: {type: String},
     email: {type: String},
-    birthDate: {type: String},
+    birthDate: {type: Date},
     document: {type: String},
-    registrationDate: {type: String},
+    registrationDate: {type: Date},
     sex: {type: String},
     knowFrom: {type: String},
-    parentAgreed: {type: Number}
+    parentAgreed: {type: Boolean},
+    subscriptions: {type: SubscriptionSchema},
+    serviceHistory: {type: ServiceHistorySchema}
 
 }, { timestamps: true });
 
 
-const Client = model("Client", clientSchema);
+const Client = model("Client", ClientSchema);
 export default Client;
